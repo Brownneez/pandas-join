7ddb1429c450a2611b470730f98421c68c172c5f
yonatan <yonatan.idlis@gmail.com>
2022-05-29 00:47:04 +0300

    answers

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from typing import Union
+
 import pandas as pd
+from pandas.core.arraylike import OpsMixin
+from pandas.core.generic import NDFrame
 
 people_df = pd.read_excel('people.xlsx')
 animals_df = pd.read_excel('animals.xlsx')
@@ -7,7 +11,7 @@ animals_df = pd.read_excel('animals.xlsx')
 people_table = people_df.rename(columns={'ID': 'personid', 'Name': 'personname', 'Age': 'personage'})
 
 # result for 2
-animals_table = animals_df.rename(columns={'ID': 'animalsid', 'Name': 'animalsname', 'Age': 'animalsage'})
+animals_table: Union[NDFrame, OpsMixin, None] = animals_df.rename(columns={'ID': 'animalsid', 'Name': 'animalsname', 'Age': 'animalsage'})
 
 # result for 3
 people_with_animals = people_table.merge(animals_table, left_on='personid', right_on='Owner_ID')
@@ -65,6 +69,14 @@ df15 = people_with_animals.loc[
 df16 = people_with_animals.loc[
     people_with_animals['personid'] == people_with_animals['animalsid']
 ]
+# result for 18
+df18 = animals_table.loc[
+    ~animals_table['Owner_ID'].isin(animals_table)
+][['animalsname', 'animalsage']]
+
+# result for 19
+#count_owner_id = people_with_animals['Owner_ID'].count()
+#df19 = count_owner_id.groupby('personid')
 
 
 # result for 21
@@ -72,6 +84,29 @@ df21 = animals_table.sort_values('animalsage', ascending=False)
 
 # result for 22
 df22 = animals_table.sort_values('animalsage', ascending=False).head()
+
+# result for 23
+twelve_oldest = animals_table.sort_values('animalsage', ascending=False).head(12)
+df23 = twelve_oldest.loc[animals_table['animalsname'] !='Ido']['animalsname']
+
+#result for 24
+youngest_white = animals_table.sort_values('animalsage', ascending=True).head(1)
+df24 = youngest_white.loc[animals_table['Color'] == 'white']
+
+# result for 25
+oldest_man = people_with_animals.sort_values('personage', ascending=False).head(1)
+df25 = oldest_man.loc[people_with_animals['Gender'] == 'M']['personname']
+
+# result for 26
+df26 = animals_table.sort_values('animalsage', ascending=False).head(1)['animalsname']
+
+# result for 27
+twelve_oldest_2 = animals_table.sort_values('animalsage', ascending=False).head(12)
+df27 = twelve_oldest_2.loc[animals_table['animalsname'] =='Ido']['animalsname']
+print(df27)
+
+
+
 
 # result for 29
 df29 = people_with_animals.sort_values('animalsage', ascending=False).groupby('personid').head(1)
